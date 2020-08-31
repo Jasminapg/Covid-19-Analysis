30ce3a955c922cf0b54fe9bdb0f5acf245c80c3a
cliffckerr <questionably@gmail.com>
2020-08-31 13:37:57 -0700

    there, but bug

--- uk_tti_movie_plot.py
+++ uk_tti_movie_plot.py
@@ -32,29 +32,31 @@ if plot_diagnostic:
         for j in range(seeds):
             pl.plot(s0.tvec, results[i,j,:])
 
+xlims = [s0.tvec[0], s0.tvec[-1]]
+ylims = [0, results.max()]
+
+mintest = 0.0
+maxtest = 0.2
+test_vals = np.linspace(mintest, maxtest, nsims) # TODO: remove duplication
+
 if plot_movie:
-    nframes = 100 # Set the number of frames
-    ndots = 100 # Set the number of dots
-    axislim = 5*pl.sqrt(nframes) # Pick axis limits
-    dots = pl.zeros((ndots, 2)) # Initialize the dots
-    frames = [] # Initialize the frames
-    old_dots = sc.dcp(dots) # Copy the dots we just made
     fig = pl.figure(figsize=(10,8)) # Create a new figure
-    for i in range(nframes): # Loop over the frames
-        dots += pl.randn(ndots, 2) # Move the dots randomly
-        color = pl.norm(dots, axis=1) # Set the dot color
-        old = pl.array(old_dots) # Turn into an array
-        plot1 = pl.scatter(old[:,0], old[:,1], c='k') # Plot old dots in black
-        plot2 = pl.scatter(dots[:,0], dots[:,1], c=color) # Note: Frames will be separate in the animation
-        pl.xlim((-axislim, axislim)) # Set x-axis limits
-        pl.ylim((-axislim, axislim)) # Set y-axis limits
+    frames = [] # Initialize the frames
+    for i in range(nsims): # Loop over the frames
+        handles = []
+        for j in range(seeds):
+            plt = pl.plot(s0.tvec, results[i,j,:])
+            handles.append(plt)
+        pl.xlim(xlims) # Set x-axis limits
+        pl.ylim(ylims) # Set y-axis limits
+        test_pct = test_vals[i]*100
         kwargs = {'transform':pl.gca().transAxes, 'horizontalalignment':'center'} # Set the "title" properties
-        title = pl.text(0.5, 1.05, f'Iteration {i+1}/{nframes}', **kwargs) # Unfortunately pl.title() can't be dynamically updated
-        pl.xlabel('Latitude') # But static labels are fine
-        pl.ylabel('Longitude') # Ditto
-        frames.append((plot1, plot2, title)) # Store updated artists
-        old_dots = pl.vstack([old_dots, dots]) # Store the new dots as old dots
-    sc.savemovie(frames, 'fleeing_dots.mp4', fps=20, quality='high') # Save movie as a high-quality mp4
+        title = pl.text(0.5, 1.05, f'Testing rate: {test_pct:0.1f}%', **kwargs) # Unfortunately pl.title() can't be dynamically updated
+        handles.append(title)
+        pl.xlabel('Date')
+        pl.ylabel('New infections')
+        frames.append(handles) # Store updated artists
+    sc.savemovie(frames, 'uk_tti_movie.mp4', fps=3, quality='high') # Save movie as a high-quality mp4
 
 
 
