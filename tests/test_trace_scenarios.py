8fcc528e9eb19e876b994de38ad5587f14fa1d2f
William Waites <ww@groovy.net>
2020-09-03 13:22:01 +0100

    refactor functional tests

---
+++ tests/test_trace_scenarios.py
@@ -0,0 +1,34 @@
+import os
+import sciris as sc
+import numpy as np
+import covasim as cv
+
+t = cv.daydiff('2020-01-21', '2020-09-01')
+
+scenarios = ['no_masks', 'low_comp', 'med_comp', 'high_comp', 'low_comp_notschools', 'med_comp_notschools', 'high_comp_notschools']
+
+testcases = [
+## (scenario, trace, test, infections)
+    (0, 0.47, 0.03, 60000),
+    (0, 0.47, 0.12, 2000),
+    (1, 0.47, 0.02, 110000),
+    (1, 0.47, 0.12, 4000),
+]
+
+for scenario, trace, test, expected in testcases:
+    ## run the simulation
+    cmd = "python UK_Test-Trace_phaseplots_26August.py --samples 2 --scenario %s --test %.02f --trace %.02f" % (scenario, test, trace)
+    os.system(cmd)
+
+    ## load the results
+    outfile = "%s/test%.02f-trace%.02f.obj" % (scenarios[scenario], test, trace)
+    results = sc.loadobj(outfile)
+
+    ## ensure that we get at least some infections
+    if not np.any(results["msim"]["new_infections"]):
+        raise ValueError("%s got no infections at all, how strange\ncommand: %s" % (scenarios[scenario], cmd))
+
+    ## ensure that the expected value of infections after september is within 10% of expected
+    infections = max(results["msim"]["new_infections"][t:])
+    if infections < 0.9*expected or infections > 1.1*expected:
+        raise ValueError("%s: with test=%s and trace=%s expected %s peak infections and got %s\ncommand: %s" % (scenarios[scenario], test, trace, expected, infections, cmd))
