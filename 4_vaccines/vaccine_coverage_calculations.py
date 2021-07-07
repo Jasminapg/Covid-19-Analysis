44c29ed5244e0657b9c5621aec738635a257d5bb
cliffckerr <questionably@gmail.com>
2021-07-07 10:02:51 -0400

    fix typo

--- 4_vaccines/vaccine_coverage_calculations.py
+++ 4_vaccines/vaccine_coverage_calculations.py
@@ -7,7 +7,7 @@ sim.initialize()
 
 data = [
     [ 0, 12,  0,  0,  0],
-    [12, 17, 10, 60, 90],
+    [12, 18, 10, 60, 90],
     [18, 40, 90, 70, 90],
     [40, 60, 90, 90, 90],
     [60, 99, 95, 95, 95],
@@ -18,11 +18,14 @@ data = np.array(data)
 n_scens = data.shape[1]-2
 results = np.zeros(n_scens)
 
+denom = sum(sim.people.age >= 12)
+
 for row in data:
     matches = (sim.people.age >= row[0]) * (sim.people.age < row[1])
+    print(row[0], row[1], sum(matches))
     for i in range(n_scens):
         results[i] += sum(matches)*row[i+2]/100
 
 print('Counts:')
 for i in range(n_scens):
-    print(f'Coverage for scenario {i+1}: {results[i]/pop_size*100:0.1f}%')
+    print(f'Coverage for scenario {i+1}: {results[i]/denom*100:0.1f}%')
